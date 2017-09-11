56300d7ee4138113e3bb1e246f304801e33a2c3b
Claudinei B. Junior <claudineibjr@hotmail.com>
2017-09-11 13:43:25 -0300

    Documentação do código

--- app/src/main/java/chatapp/com/claudineibjr/androidchatapp/Login.java
+++ app/src/main/java/chatapp/com/claudineibjr/androidchatapp/Login.java
@@ -1,9 +1,10 @@
 package chatapp.com.claudineibjr.androidchatapp;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,7 +22,7 @@ import java.util.ArrayList;
 
 public class Login extends AppCompatActivity {
 
-    //Firebase Authentication
+    //Firebase Autenticação
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
@@ -44,13 +45,18 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Assim que a tela iniciar, prepara a autenticação
         preparaAutenticacao();
 
+        //Prepara as abas contidas no TabHost
         preparaAbas();
+
+        //Instancia os elementos visuais, permitindo que recebam e tenham valor atribuído
         instanciaElementosVisuais();
 
     }
 
+    //Função acionada quando o app é iniciado
     @Override
     protected void onStart() {
         super.onStart();
@@ -61,6 +67,7 @@ public class Login extends AppCompatActivity {
         }
     }
 
+    //Função acionada quando o app é fechado
     @Override
     protected void onStop() {
         super.onStop();
@@ -74,49 +81,59 @@ public class Login extends AppCompatActivity {
 
     }
 
+    //Função responsável por preparar o Listener da autenticação
     private void preparaAutenticacao() {
 
         try{
 
+            //Inicia o Firebase
             FirebaseApp.initializeApp(this);
 
+            //Recebe a instância do Firebase
             mAuth = FirebaseAuth.getInstance();
 
+            //Listener que ficará "ouvindo" alterações na autenficação
             mAuthListener = new FirebaseAuth.AuthStateListener() {
                 @Override
                 public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
 
+                    // Quando a autenticação mudar, recebe o usuário vindo do Firebase e verifica se é diferente de nulo
+                    //  significando que está autenciado
                     FirebaseUser user = firebaseAuth.getCurrentUser();
                     if (user != null) {
 
+                        //Cria o usuário do Firebase
                         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
                         Usuario usuarioLogado = null;
 
+                        //Confirme se o usuário do Firebase é diferente de nulo
                         if (firebaseUser != null) {
+
+                            //Caso o usuário do Firebase for diferente de nulo, cria o objeto do usuário na aplicação
                             usuarioLogado = new Usuario(firebaseUser.getUid(), firebaseUser.getEmail());
 
                             //Indo para a próxima tela
                             Intent intent = new Intent(Login.this, MainActivity.class);
+
+                            //Envia parâmetros extras para a próxima tela
                             intent.putExtra("usuarioLogado", usuarioLogado);
                             intent.putExtra("cadastro", cadastro);
 
                             startActivity(intent);
 
                         }
-
-                        // User is signed in
-                        //Toast.makeText(getApplicationContext(), "onAuthStateChanged:signed_in:" + user.getUid(), Toast.LENGTH_SHORT).show();
-                    } else {
-                        // User is signed out
-                        //Toast.makeText(getApplicationContext(), "onAuthStateChanged:signed_out", Toast.LENGTH_SHORT).show();
                     }
                 }
             };
         } catch (Exception e){
-            Toast.makeText(getApplicationContext(), "preparaAutenticacao\n\n" + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+            //Caso tenha dado erro, exibe o erro na tela
+            Toast.makeText(getApplicationContext(), getClass().toString() + "\n" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.d(getClass().toString(), e.getMessage());
         }
     }
 
+    //Função responsável por instanciar os elementos que o usuário vê
     private void instanciaElementosVisuais() {
         //Campos da tela de login
         txtLoginEmail = (EditText) findViewById(R.id.txtLoginEmail);
@@ -147,52 +164,72 @@ public class Login extends AppCompatActivity {
 
     }
 
+    //Função acionada quando for fazer o cadastro
     private void cadastrar(final String email, String nome, final String senha) {
-        ArrayList<EditText> campos = new ArrayList<>(); campos.add(txtCadastroEmail);  campos.add(txtCadastroNome); campos.add(txtCadastroSenha);
+        //Cria e popula um array com os campos que devem ser preenchidos
+        ArrayList<EditText> campos = new ArrayList<>();
+        campos.add(txtCadastroEmail);
+        campos.add(txtCadastroNome);
+        campos.add(txtCadastroSenha);
+
+        //Verifica se todos os campos estão preenchidos
         if (camposPreenchidos(campos)){
 
+            //Cria um listener que ficará ouvindo se a tentativa de criação do usuário foi bem sucedida
             mAuth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    // If sign in fails, display a message to the user. If sign in succeeds
-                    // the auth state listener will be notified and logic to handle the
-                    // signed in user can be handled in the listener.
                     if (!task.isSuccessful()) {
-                        Toast.makeText(getApplicationContext(), "Autenticação falhou\n\n" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        //Caso tenha dado erro na autenticação, notifica ao usuário
+                        Toast.makeText(getApplicationContext(), getClass().toString() + "\n" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.d(getClass().toString(), task.getException().getMessage());
                     }else{
+
+                        //Caso tenha dado certo a criação, seta que é um cadastro e então o login será feito
+                        //  automaticamente através do Listener da autenticação
                         cadastro = true;
-                        //Toast.makeText(getApplicationContext(), "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
                     }
                 }
             });
         }else{
+            //Caso não tenha preenchido todos os campos notifica ao usuário
             Toast.makeText(getApplicationContext(), "Por favor, preencha todos os campos", Toast.LENGTH_SHORT).show();
         }
     }
 
+    //Função acionada quando for fazer o login
     private void login(String email, String senha) {
-        ArrayList<EditText> campos = new ArrayList<>(); campos.add(txtLoginEmail);  campos.add(txtLoginSenha);
-        if (camposPreenchidos(campos) == true){
+        //Cria e popula um array com os campos que devem ser preenchidos
+        ArrayList<EditText> campos = new ArrayList<>();
+        campos.add(txtLoginEmail);
+        campos.add(txtLoginSenha);
 
+        //Verifica se todos os campos estão preenchidos
+        if (camposPreenchidos(campos)){
+
+            //Cria um listener que ficará ouvindo se a tentativa de login do usuário foi bem sucedida
             mAuth.signInWithEmailAndPassword(email, senha)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-                            // If sign in fails, display a message to the user. If sign in succeeds
-                            // the auth state listener will be notified and logic to handle the
-                            // signed in user can be handled in the listener.
-                            if (!task.isSuccessful())
-                                Toast.makeText(getApplicationContext(), "signInWithEmail:failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            if (!task.isSuccessful()) {
+                                //Caso tenha dado erro na autenticação, notifica ao usuário
+                                Toast.makeText(getApplicationContext(), getClass().toString() + "\n" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                Log.d(getClass().toString(), task.getException().getMessage());
+                            }
                         }
                     });
 
         }else{
+            //Caso não tenha preenchido todos os campos notifica ao usuário
             Toast.makeText(getApplicationContext(), "Por favor, preencha todos os campos", Toast.LENGTH_SHORT).show();
         }
     }
 
+    //Função que verifica se todos os campos que deveriam ter sido preenchidos foram efetivamente preenchidos
     public boolean camposPreenchidos(ArrayList<EditText> campos){
 
+        //Faz a varredura em todos os campos para ver se estão preenchidos
         for (int iCount = 0; iCount < campos.size(); iCount++){
             if (campos.get(iCount).getText().toString().equals("")){
                 return false;
@@ -202,20 +239,24 @@ public class Login extends AppCompatActivity {
         return true;
     }
 
+    //Função responsável por preparar as abas
     private void preparaAbas() {
+        //Cria e configura o TabHost
         TabHost abas = (TabHost) findViewById(R.id.loginTabHost);
         abas.setup();
-
         TabHost.TabSpec descritor = abas.newTabSpec("aba1");
 
+        //Cria a primeira aba, do login
         descritor.setContent(R.id.tabLogin);
         descritor.setIndicator("Login");
         abas.addTab(descritor);
 
+        //Cria a segunda aba, do cadastro
         descritor = abas.newTabSpec("aba2");
         descritor.setContent(R.id.tabCadastro);
         descritor.setIndicator("Cadastro");
 
+        //Adiciona as abas ao TabHost
         abas.addTab(descritor);
     }
 
--- app/src/main/java/chatapp/com/claudineibjr/androidchatapp/MainActivity.java
+++ app/src/main/java/chatapp/com/claudineibjr/androidchatapp/MainActivity.java
@@ -35,6 +35,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Classe que fará a adaptação dos contatos para um ListView personalizado
     public class ContatosAdapter extends BaseAdapter {
 
         private final ArrayList<DadosUsuario> contatos;
@@ -108,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Classe que fará a adaptação das conversas recentes para um ListView personalizado
     public class ConversasAdapter extends BaseAdapter {
 
         private final ArrayList<ConversaRecente> conversasRecentes;
@@ -168,13 +170,22 @@ public class MainActivity extends AppCompatActivity {
 
     private Usuario usuario;
 
+    //Array que conterá os contatos exibidos na lista
     private ArrayList<DadosUsuario> contatos = new ArrayList<>();
+
+    //Array que conterá as conversas recentes exibidos na lista
     private ArrayList<ConversaRecente> conversas = new ArrayList<>();
 
+    //Lista de contatos
     private ListView listaContatos;
+
+    //Lista de conversas recentes
     private ListView listaConversas;
 
+    //ArrayAdapter para exibir os contatos no ListView Personalizado
     private ContatosAdapter usuariosArrayAdapter;
+
+    //ArrayAdapter para exibir as conversas recentes no ListView Personalizado
     private ConversasAdapter conversasArrayAdapter;
 
     @Override
@@ -182,7 +193,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Prepara as abas contidas no TabHost
         preparaAbas();
+
+        //Instancia os elementos visuais, permitindo que recebam e tenham valor atribuído
         instanciaElementosVisuais();
 
         //Verifica se existem dados vindouros da tela anterior
@@ -192,39 +206,63 @@ public class MainActivity extends AppCompatActivity {
             // Recebe o usuário da tela anterior
             usuario = (Usuario) extra.getSerializable("usuarioLogado");
 
+            //Caso for um cadastro
             if (extra.getBoolean("cadastro")){
 
+                //Seta que o usuário está online
                 usuario.getDadosUsuario().setConectado(true);
+
+                //Seta que ele foi "visto" pela última vez agora
                 usuario.getDadosUsuario().setUltimaVez(Calendar.getInstance(new Locale("pt", "br")).getTime());
 
                 try{
+
+                    //Cria o usuário dentro do Bando de dados
                     Parametros.getUsuarioReferencia().child(usuario.getUid()).setValue(usuario);
                 } catch (Exception e){
-                    Toast.makeText(getApplicationContext(), "Não deu\n\n" + e.getMessage(), Toast.LENGTH_LONG).show();
+
+                    //Exibe o erro caso tenha dado errado a criação do usuário no banco de dados
+                    Toast.makeText(getApplicationContext(), getClass().toString() + "\n" + e.getMessage(), Toast.LENGTH_LONG).show();
+                    Log.d(getClass().toString(), e.getMessage());
                 }
 
             }else{
+                //Caso o usuário não esteja sendo criado, busca seus dados no banco de dados
                 Parametros.getUsuarioReferencia().orderByKey().equalTo(usuario.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         for (DataSnapshot data : dataSnapshot.getChildren()) {
 
+                            //Instancia o usuário vindouro do banco de dados
                             usuario = data.getValue(Usuario.class);
+
+                            //Seta que o usuário está online
                             usuario.getDadosUsuario().setConectado(true);
+
+                            //Seta que ele foi "visto" pela última vez agora
                             usuario.getDadosUsuario().setUltimaVez(Calendar.getInstance(new Locale("pt", "br")).getTime());
 
+                            //Carrega as conversas do usuário
                             carregaConversas();
+
+                            //Carrega os contatos do usuário
                             carregaContatos();
 
                             try{
+
+                                //Insere as alterações realizadas no usuário num HashMap
                                 Map<String, Object> atualizacoes = new HashMap<>();
                                 atualizacoes.put("/dadosUsuario/conectado", usuario.getDadosUsuario().isConectado());
                                 atualizacoes.put("/dadosUsuario/ultimaVez", usuario.getDadosUsuario().getUltimaVez());
 
+                                //Tenta atualizar os dados do usuário no banco de dados
                                 Parametros.getUsuarioReferencia().child(usuario.getUid()).updateChildren(atualizacoes);
 
                             } catch (Exception e){
-                                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                                //Exibe o erro caso tenha dado errado a criação do usuário no banco de dados
+                                Toast.makeText(getApplicationContext(), getClass().toString() + "\n" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                Log.d(getClass().toString(), e.getMessage());
                             }
 
                         }
@@ -241,19 +279,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //Função responsável por instanciar os elementos que o usuário vê
     private void instanciaElementosVisuais() {
         listaContatos = (ListView) findViewById(R.id.lista_contatos);
         listaConversas = (ListView) findViewById(R.id.lista_conversas);
     }
 
+    //Função responsável por carregar os contatos
     private void carregaContatos() {
 
+        //Instancia o ArrayAdapter de usuários para a lista de contatos
         usuariosArrayAdapter = new ContatosAdapter(contatos, MainActivity.this);
+
+        //Seta o ArrayAdapter para a lista de contatos
         listaContatos.setAdapter(usuariosArrayAdapter);
 
+        //Para cada contato encontrado (uid), vai buscar no banco de dados as informaões deste contato
         for (Iterator<String> lstContato = usuario.getContatos().iterator(); lstContato.hasNext();){
             String strContato = lstContato.next();
 
+            //Vai buscar no banco de dados a informação dos dados do usuário
             Parametros.getUsuarioReferencia().child(strContato).addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
@@ -273,21 +318,30 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
+        //Notifica ao ArrayAdapter que o DataSet foi alterado
         usuariosArrayAdapter.notifyDataSetChanged();
 
     }
 
+    //Função responsável por carregar as conversas recentes
     private void carregaConversas() {
 
+        //Instancia o ArrayAdapter de conversas para a lista de conversas recentes
         conversasArrayAdapter = new ConversasAdapter(conversas, MainActivity.this);
+
+        //Seta o ArrayAdapter para a lista de contatos
         listaConversas.setAdapter(conversasArrayAdapter);
 
+        //Cria variáveis que receberão o uid do Contato e a key da conversa
         String uidContato, uidConversa;
 
+        //Para cada objeto do HashMap das converas recentes, faz uma iteração para obter um contato
         for(Object objname: usuario.getConversasRecentes().keySet()) {
-            Log.d(getClass().toString() + "Conversas", "Chave: " + objname.toString());
+
+            //Seta o uid do contato
             uidContato = objname.toString();
 
+            //Vai buscar no banco de dados o e-mail do contato
             Parametros.getUsuarioReferencia()
                     .child(uidContato)
                     .child(Parametros.getDadosUsuario())
@@ -304,21 +358,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
-            //Destinatário = objname.toString();
+            //Para cada objeto do HashMap das converas recentes, faz uma iteração para obter uma conversa
             for(Object objname2: usuario.getConversasRecentes().get(objname).keySet()) {
-                Log.d(getClass().toString() + "Conversas", "Valor: " + usuario.getConversasRecentes().get(objname).get(objname2).toString());
+                Log.d(getClass().toString() + "Conversas", "Valor: " + usuario.getConversasRecentes().get(objname).get(objname2));
 
-                uidConversa = usuario.getConversasRecentes().get(objname).get(objname2).toString();
+                //Recebe o uid da conversa do HashMap
+                uidConversa = usuario.getConversasRecentes().get(objname).get(objname2);
 
+                //Cria a Key da conversa que será o uid dos dois contatos ordenado alfabéticamente
                 String keyConversa;
-
                 if (usuario.getUid().compareToIgnoreCase(uidContato) > 0)
                     keyConversa = uidContato + "-" + usuario.getUid();
                 else
                     keyConversa = usuario.getUid() + "-" + uidContato;
 
-                Parametros
-                        .getMensagensReferencia()
+                //Vai buscar no banco de dados os dados da última conversa
+                Parametros.getMensagensReferencia()
                         .child(keyConversa)
                         .child(uidConversa)
                         .addListenerForSingleValueEvent(new ValueEventListener() {
@@ -337,10 +392,12 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        //Notifica ao ArrayAdapter que o DataSet foi alterado
         conversasArrayAdapter.notifyDataSetChanged();
 
     }
 
+    //Função responsável por preparar as abas
     private void preparaAbas() {
         TabHost abas = (TabHost) findViewById(R.id.mainTabHost);
         abas.setup();
@@ -358,34 +415,53 @@ public class MainActivity extends AppCompatActivity {
         abas.addTab(descritor);
     }
 
+    //Função acionada quando as opções do menu forem criadas
     @Override
     public boolean onCreateOptionsMenu(Menu _menu) {
+
+        //Função responsável por setar o menu que conterá o botão de desconectar
         super.onCreateOptionsMenu(_menu);
 
         getMenuInflater().inflate(R.menu.menuprincipal, _menu);
         return (true);
     }
 
+    // Função responsável por desconectar o usuário
     public void desconecta(){
-        // Desconecta o usuário
 
+        //Seta que o usuário está off-line
         usuario.getDadosUsuario().setConectado(false);
+
+        //Seta que ele foi "visto" pela última vez agora
         usuario.getDadosUsuario().setUltimaVez(Calendar.getInstance(new Locale("pt", "br")).getTime());
 
+        //Insere as alterações realizadas no usuário num HashMap
         Map<String, Object> atualizacoes = new HashMap<>();
         atualizacoes.put("/dadosUsuario/conectado", usuario.getDadosUsuario().isConectado());
         atualizacoes.put("/dadosUsuario/ultimaVez", usuario.getDadosUsuario().getUltimaVez());
 
-        Parametros.getUsuarioReferencia().child(usuario.getUid()).updateChildren(atualizacoes);
+        try{
+            //Tenta atualizar os dados do usuário no banco de dados
+            Parametros.getUsuarioReferencia().child(usuario.getUid()).updateChildren(atualizacoes);
+        }catch (Exception e){
 
-        //Parametros.getUsuarioReferencia().child(usuario.getUid()).setValue(usuario);
+            //Exibe o erro caso tenha dado errado a atualização do usuário no banco de dados
+            Toast.makeText(getApplicationContext(), getClass().toString() + "\n" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.d(getClass().toString(), e.getMessage());
+        }
 
+        //Notifica que o usuário foi desautenticado
         FirebaseAuth.getInstance().signOut();
+
+        //Finaliza a activity
         finish();
     }
 
+    //Função responsável por identificar o clique do usuário
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+        //Deteca o botão clicado
         switch (item.getItemId()) {
             case R.id.menu_add_contato:{
                 novoContato();
@@ -400,10 +476,14 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    //Função responsável por exibir uma caixa de diálogo na tela para a criação do contato
     private void novoContato() {
+
+        //Cria um campo que será exibido para o usuário
         final EditText txtNovoContato = new EditText(this);
         txtNovoContato.setHint("Exemplo: claudineibjr@hotmail.com");
 
+        //Cria um alerta que irá exibir o campo para informação do e-mail do usuário
         new AlertDialog.Builder(this)
                 .setTitle("Novo contato")
                 .setMessage("Digite o e-mail do novo contato")
@@ -411,35 +491,49 @@ public class MainActivity extends AppCompatActivity {
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        //Ao clicar no botão ok, tenta criar este novo contato
                         adicionaContato(txtNovoContato.getText().toString());
                     }
                 }).show();
     }
 
+    //Função responsável por receber um e-mail e adicionar este e-mail na lista de contatos do usuário
     private void adicionaContato(String strNovoContato) {
+        //Cria uma query para buscar os dados no banco de dados
         Query queryEmail = Parametros.getUsuarioReferencia()
                 .orderByChild("dadosUsuario/email")
                 .equalTo(strNovoContato)
                 .limitToFirst(1);
 
+        //Cria um LIstener unitário que irá receber os dados do usuário
         queryEmail.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot data : dataSnapshot.getChildren()) {
+
+                    //Quando receber os dados do usuário, pega a Key e adiciona-a a lista do usuário
                     usuario.addContato(data.getKey());
 
                     for (DataSnapshot dataChild : data.getChildren()){
                         if (dataChild.getKey().equals("dadosUsuario")){
+                            //Adicionar os dados do usuário a lista de contato
                             contatos.add(dataChild.getValue(DadosUsuario.class));
+
+                            //Notifica o ArrayAdapter que o DataSet foi alterado
                             usuariosArrayAdapter.notifyDataSetChanged();
                             break;
                         }
                     }
 
                     try {
+
+                        //Tenta atualizar os dados do usuário no banco de dados
                         Parametros.getUsuarioReferencia().child(usuario.getUid()).child("contatos").setValue(usuario.getContatos());
                     } catch (Exception e){
-                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                        //Exibe o erro caso tenha dado errado a atualização do usuário no banco de dados
+                        Toast.makeText(getApplicationContext(), getClass().toString() + "\n" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.d(getClass().toString(), e.getMessage());
                     }
                 }
             }
@@ -451,17 +545,30 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //Função acionada quando o app é fechado
     @Override
     protected void onStop() {
 
+        //Seta que o usuário está off-line
         usuario.getDadosUsuario().setConectado(false);
+
+        //Seta que ele foi "visto" pela última vez agora
         usuario.getDadosUsuario().setUltimaVez(Calendar.getInstance(new Locale("pt", "br")).getTime());
 
+        //Insere as alterações realizadas no usuário num HashMap
         Map<String, Object> atualizacoes = new HashMap<>();
         atualizacoes.put("/dadosUsuario/conectado", usuario.getDadosUsuario().isConectado());
         atualizacoes.put("/dadosUsuario/ultimaVez", usuario.getDadosUsuario().getUltimaVez());
 
-        Parametros.getUsuarioReferencia().child(usuario.getUid()).updateChildren(atualizacoes);
+        try {
+
+            //Tenta atualizar os dados do usuário no banco de dados
+            Parametros.getUsuarioReferencia().child(usuario.getUid()).updateChildren(atualizacoes);
+        } catch (Exception e){
+            //Exibe o erro caso tenha dado errado a atualização do usuário no banco de dados
+            Toast.makeText(getApplicationContext(), getClass().toString() + "\n" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.d(getClass().toString(), e.getMessage());
+        }
 
         super.onStop();
     }
